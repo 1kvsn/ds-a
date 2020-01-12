29006829027e61240f12eab3ec5e64a1e0fd8773
sasikant <nairs999@gmail.com>
2020-01-13 01:46:34 +0530

    Add tree and tree traversing algos

--- trees/BinarySearchTree.js
+++ trees/BinarySearchTree.js
@@ -1,99 +1,154 @@
-	// Binary Tree
+// Binary Tree
 
-	// Root has max 2 nodes and each node can have max 2 children or no children.
+// Root has max 2 nodes and each node can have max 2 children or no children.
 
-	// ------------------------------------------------------------
+// ------------------------------------------------------------
 
-	// Binary Search Tree (BST)
+// Binary Search Tree (BST)
 
-	// Every piece of data is stored in a specific order.
+// Every piece of data is stored in a specific order.
 
-	// BST Rules:
-	// Every parent node has at most TWO children.
-	// Every node to the left of a parent node is ALWAYS LESS then the parent.
-	// Every node to the right of a parent node is ALWAYS GREATER than the parent.
+// BST Rules:
+// Every parent node has at most TWO children.
+// Every node to the left of a parent node is ALWAYS LESS then the parent.
+// Every node to the right of a parent node is ALWAYS GREATER than the parent.
 
-	// ------------------------------------------------------------
+// ------------------------------------------------------------
 
-	// Big O of BST
+// Big O of BST
 
-	// Insertion - O(log n) # which is pretty good
-	// Searching - O(log n)
+// Insertion - O(log n) # which is pretty good
+// Searching - O(log n)
+
+// ------------------------------------------------------------
 
 
-	class Node {
-		constructor(value) {
-			this.value = value;
-			this.left = null;
-			this.right = null;
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+	
+	insert(value) {
+		var newNode = new Node(value);
+		if (this.root === null) {
+			this.root = newNode;
+			return this;
+		}
+		// else we begin from root similar to how we begin from head in LinkedList
+		var current = this.root;
+		while (true) {
+			if (value === current.value) return undefined;
+			if (value < current.value) {
+				if (current.left === null) {
+					current.left = newNode;
+					return this;
+				}
+				// else if there is a node on the LEFT, switch to it and while loop will repeat process again
+				current = current.left;
+			} else {
+				if (current.right === null) {
+					current.right = newNode;
+					return this;
+				}
+				// else if there is a node on the RIGHT, switch to it and while loop will repeat process again
+				current = current.right;
+			}
 		}
 	}
 
-	class BinarySearchTree {
-		constructor() {
-			this.root = null;
-		}
-		insert(value){
-				var newNode = new Node(value);
-        if(this.root === null){
-            this.root = newNode;
-            return this;
-				}
-				// else we begin from root similar to how we begin from head in LinkedList
-        var current = this.root;
-        while(true){
-            if(value === current.value) return undefined;
-            if(value < current.value){
-                if(current.left === null){
-                    current.left = newNode;
-                    return this;
-								}
-								// else if there is a node on the LEFT, switch to it and while loop will repeat process again
-                current = current.left;
-            } else {
-                if(current.right === null){
-                    current.right = newNode;
-                    return this;
-								} 
-								// else if there is a node on the RIGHT, switch to it and while loop will repeat process again
-                current = current.right;
-            }
-        }
-		}
-		find(value) {
-			if (this.root === null) return false;
-			var current = this.root,
-				found = false;
-			// while there is something to loopover and until we've found the value
-			while (current && !found) {
-				if (value < current.value) {
-					current = current.left;
-				} else if (value > current.value) {
-					current = current.right;
-				} else {
-					// runs when both the above conditions are false i.e when value is found
-					found = true;
-				}
+	find(value) {
+		if (this.root === null) return false;
+		var current = this.root,
+			found = false;
+		// while there is something to loopover and until we've found the value
+		while (current && !found) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value > current.value) {
+				current = current.right;
+			} else {
+				// runs when both the above conditions are false i.e when value is found
+				found = true;
 			}
-			if (!found) return undefined;
-			return current;
 		}
-		contains(value) {
-			if (this.root === null) return false;
-			var current = this.root,
-				found = false;
-			while (current && !found) {
-				if (value < current.value) {
-					current = current.left;
-				} else if (value > current.value) {
-					current = current.right;
-				} else {
-					return true;
-				}
-			}
-			return false;
-		}
+		if (!found) return undefined;
+		return current;
 	}
+
+	contains(value) {
+		if (this.root === null) return false;
+		var current = this.root,
+			found = false;
+		while (current && !found) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value > current.value) {
+				current = current.right;
+			} else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	BFS() {
+		var node = this.root,
+			data = [],
+			queue = [];
+		queue.push(node);
+		while (queue.length) {
+			node = queue.shift();
+			data.push(node.value); // the value is pushed so it is easier to see the output, otherwise push node only
+			// since this is a binary tree, we're looking for left and right only. For othe types of trees, we could've added loop or recursion 
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
+		}
+		return data;
+	}
+
+	DFSPreOrder() {
+		var data = [];
+		function traverse(node) {
+			// value is pushed to easier output read
+			data.push(node.value);
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+		}
+		// start traversing from beginning of tree
+		traverse(this.root);
+		return data;
+	}
+
+	DFSPostOrder() {
+		var data = [];
+		function traverse(node) {
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+			data.push(node.value); // just this line position changed from PreOrder
+		}
+		traverse(this.root);
+		return data;
+	}
+
+	DFSInOrder() {
+		var data = [];
+		function traverse(node) {
+			if (node.left) traverse(node.left);
+			data.push(node.value); // just this line position changed from PostOrder
+			if (node.right) traverse(node.right);
+		}
+		traverse(this.root);
+		return data;
+	}
+}
 
 //      10
 //   5     13
@@ -107,5 +162,10 @@ tree.insert(11)
 tree.insert(2)
 tree.insert(16)
 tree.insert(7)
+
+tree.DFSPreOrder();	 // [10, 5, 2, 7, 13, 11, 16]
+tree.DFSPostOrder(); // [2, 7, 5, 11, 16, 13, 10]
+tree.DFSInOrder();  //  [2, 5, 7, 10, 11, 13, 16]
+
 
 
--- trees/trees.js
+++ trees/trees.js
@@ -27,3 +27,30 @@
 
 	// ------------------------------------------------------------
 
+// Tree Traversal
+
+// 2 ways of tree traversal:-
+
+// Breadth-first Search
+// Depth-first Search :
+				// 1. DFS-PreOrder
+				// 2. DFS-PostOrder :-
+								// 1. First, we check if the current node has left or right
+								// 2. If left node found, check if it has left or right
+								// 3. Finish checking all the lefts and right nodes of current node until there isn't.
+								// 4. Only when there isn't any left and right, the node is visited/pushed to output array
+								// 5. Only when both the left and right of a node is visited/pushed to output array, the node itself is visited/pushed
+								// 6. So, basically it means we check for and visit all children before we visit/output the parent node
+								// 7. Becoz of this, the root node is the last output
+				// 3. DFS-InOrder :-
+
+// -----------------------------------------------------------------------
+
+// Big-O of Tree Traversal
+
+// It is NOT about time. We visit each node once. So, the time used by both methods is almost same.
+// It is about space.
+
+// In BFS, we're using a queue to store the nodes. This could use lots of space incase the tree has considerable height/depth (many levels of children-children-children)
+
+// -------------------------------------------------------------
